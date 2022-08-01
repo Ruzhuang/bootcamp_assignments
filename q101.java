111eac4b59bd2bfad1473796f45f5cd3fab4e60a
Ruzhuang <67622064+Ruzhuang@users.noreply.github.com>
2022-08-01 12:52:34 -0700

    Create q101.java

---
+++ q101.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root.left==null){
+            if(root.right==null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        if(root.right==null){
+            if(root.left==null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return compare(root.left, root.right);
+    }
+    public boolean compare(TreeNode left, TreeNode right){
+        if(left.val!=right.val){
+            return false;
+        }
+        if(left.left==null){
+            if(right.right!=null){
+                return false;
+            }
+        }
+        else{
+            if(right.right==null){
+                return false;
+            }
+            return compare(left.left,right.right);
+        }
+        if(left.right==null){
+            if(right.left!=null){
+                return false;
+            }
+        }
+        else{
+            if(right.left==null){
+                return false;
+            }
+            return compare(left.right,right.left);
+        }
+        return true;
+           
+    }
+}
